3d880cd940d569c420a8992a98ab5224023cf89e
ZhaoHuan <byzhaohuan@gmail.com>
2015-02-03 20:52:58 +0800

    完成Q12-Q18的代码

--- adaboost.py
+++ adaboost.py
@@ -10,17 +10,22 @@ import math
 max_iterations = 300
 train_data_file = '../assignment2/hw2_adaboost_train.dat'
 train_data = []
+test_data_file = '../assignment2/hw2_adaboost_test.dat'
+test_data = []
 NEG_INF = -10000
 U = []#记录每一次迭代中的u_t list iter_num * N;
 
 def load_data():
     '''
-        读入训练集，训练集格式为:
+        读入训练集和测试集，格式为:
             feature1    feature2  label
     '''
-    global train_data
+    global train_data, test_data
     lines = open(train_data_file, 'r').readlines()
     train_data = [(float(f1), float(f2), int(label)) for f1, f2, label in [l.strip().split() for l in lines]]
+    lines = open(test_data_file, 'r').readlines()
+    test_data = [(float(f1), float(f2), int(label)) for f1, f2, label in [l.strip().split() for l in lines]]
+
 
 def generate_thetas(feature_index):
     '''
@@ -52,11 +57,10 @@ def is_unequal(a, b):
     return 0 if a == b else 1
 
 def cal_Ein(u_t, error_num):
-    N = len(u_t)
     sum_v = 0.0
     for u, err in zip(u_t, error_num):
         sum_v += u * err
-    return sum_v / N
+    return sum_v / len(u_t)
 
 def cal_Eps(u_t, min_err_num):
     '''
@@ -78,11 +82,12 @@ def adaboost():
     '''
     u_t = [0.01] * 100
     #每一轮都会生成一个decision_stump，四个参数:f_i, s, theta, alpha
-    decision_stumps = []
-    iter_errs = []
+    decision_stumps, u_ts = [], []
+    iter_errs, iter_eps = [], []
     for t in range(max_iterations):
         min_err, min_err_num = 10.0, []
-        print '%d iterations...' % (t+1)
+        if t in [0, 99, 199, 299]:
+            print '%d iterations...' % (t+1)
         #decision stump的三个参数
         min_feature_index, min_s, min_theta = 0, 1, NEG_INF
         for f_i in range(2):
@@ -98,34 +103,131 @@ def adaboost():
                         error_num[ind] = is_unequal(y, row[2])#训练集中第三列为label
                     Ein = cal_Ein(u_t, error_num)
                     if is_lt(Ein, min_err):
-                        #import pdb;pdb.set_trace()
                         min_err = Ein
                         min_feature_index = f_i
                         min_s = s
                         min_theta = theta
                         min_err_num = list(error_num) #要求值传递
-        #import pdb;pdb.set_trace()
+
         Eps = cal_Eps(u_t, min_err_num)
+        iter_eps.append(Eps)
+        u_ts.append(list(u_t))#保留整个迭代过程中的u
         if not is_unequal(Eps, 1.0) or not is_unequal(Eps, 0.0):
-            import pdb;pdb.set_trace()
+            #import pdb;pdb.set_trace()
             print 'Eps equals %s, terminate' % str(Eps)
             return
 
         delta_t = (math.sqrt(Eps/(1-Eps)), math.sqrt((1-Eps)/Eps))
-        print 'Eps=%s, delta_t=%s,  u_t=%s\n' % (str(Eps), str(delta_t), str(u_t))
+        #print 'Eps=%s, delta_t=%s,  u_t=%s\n' % (str(Eps), str(delta_t), str(u_t))
 
         update_ut(Eps, delta_t, u_t, min_err_num)
+        if t == 1:
+            print '****************************************************'
+            print '                     Question 14                    '
+            print '                  U_t = sum_1^N{u_t}                '
+            print '****************************************************'
+            print 'U_2=%.5f' % sum(u_t)
+            print '****************************************************'
+            print '                     Question 14                    '
+            print '****************************************************'
+
         alpha = math.log(delta_t[1])
-        decision_stumps.append((min_feature_index, min_s, theta, alpha))
+        decision_stumps.append((min_feature_index, min_s, min_theta, alpha))
         iter_errs.append(min_err)
-    print 'Ein of all iterations: %s' % (str(iter_errs))
-    print 'decision_stumps is %s' % ('-'.join([str(r) for r in decision_stumps]))
-    #return decision_stumps
+        if t == 0:
+            print '****************************************************'
+            print '                     Question 12                    '
+            print '                       Ein(g1)                      '
+            print '****************************************************'
+            print 'Ein(g1) = %s' % min_err
+            print '****************************************************'
+            print '                     Question 12                    '
+            print '****************************************************'
+
+            print '****************************************************'
+            print '                     Question 17                    '
+            print '                  calculate Eout(g1)               '
+            print '****************************************************'
+            tmp_error_num = [0] * len(test_data)
+            for tmp_ind, tmp_row in enumerate(test_data):
+                y = min_s * sign(row[min_feature_index] - min_theta)
+                tmp_error_num[tmp_ind] = is_unequal(y, row[2])#训练集中第三列为label
+            Eout_g_1 = sum(tmp_error_num) * 1.0 / len(test_data)
+            print 'Eout(g1) = %.5f, num_of_test_data=%d' % (Eout_g_1, tmp_ind+1)
+            print '****************************************************'
+            print '                     Question 17                    '
+            print '****************************************************'
+    #print 'Ein of all iterations: %s' % (str(iter_errs))
+    #print 'decision_stumps is %s' % ('-'.join([str(r) for r in decision_stumps]))
+    return decision_stumps, u_ts, iter_eps
+
+def predict_by_decision_stumps(row, decision_stumps):
+    '''
+        row:一行训练数据
+        decision_stumps: 每一行四个参数: feature_index, s, theta, alpha
+        使用公式:
+        G(x) = sign(sum_1^T t alpha *  (s * sign(x_i - theta)))
+    '''
+    Y = 0.0
+    for f_i, s, theta, alpha in decision_stumps:
+        Y += alpha * s * sign(row[f_i] - theta)
+    return sign(Y)
+
 
 if __name__ == '__main__':
     from datetime import datetime
     print '******************%s********************' % datetime.today().strftime('%Y-%m-%d %H:%M:%S')
     load_data()
     print train_data
-    adaboost()
-    print '********************************'
+    decision_stumps, u_ts, iter_eps = adaboost()
+
+    print '****************************************************'
+    print '                     Question 13                    '
+    print '                       Ein(G)                       '
+    print '****************************************************'
+    error_num = [0] * 100
+    for ind, row in enumerate(train_data):
+        y = predict_by_decision_stumps(row, decision_stumps)
+        error_num[ind] = is_unequal(y, row[2])#训练集中第三列为label
+    Ein_G = sum(error_num) * 1.0 / len(train_data)
+    print 'Ein(G) = %.5f' % (Ein_G)
+    print '****************************************************'
+    print '                     Question 13                    '
+    print '****************************************************'
+
+    print '****************************************************'
+    print '                     Question 15                    '
+    print '                        U_T                         '
+    print '****************************************************'
+    U_T = 0.0
+    for u_t in u_ts:
+        U_T += sum(u_t)
+    print 'U_T=%.5f' % U_T
+    print '****************************************************'
+    print '                     Question 15                    '
+    print '****************************************************'
+
+    print '****************************************************'
+    print '                     Question 16                    '
+    print '                     min{Epsilon}                   '
+    print '                     Question 16                    '
+    print '****************************************************'
+    min_eps = [100.0, 0]#value, index
+    for ind, e in enumerate(iter_eps):
+        if e < min_eps[0]:
+            min_eps = (e, ind+1)
+    print 'min epsilon=%.5f(%d iteration)' % min_eps
+    print '****************************************************'
+
+    print '****************************************************'
+    print '                     Question 18                    '
+    print '                       Eout(G)                      '
+    print '****************************************************'
+    error_num = [0] * len(test_data)
+    for ind, row in enumerate(test_data):
+        y = predict_by_decision_stumps(row, decision_stumps)
+        error_num[ind] = is_unequal(y, row[2])#训练集中第三列为label
+    Eout_G = sum(error_num) * 1.0 / len(test_data)
+    print 'Eout(G) = %.5f, num_of_test_data=%d' % (Eout_G, len(test_data))
+    print '                     Question 18                    '
+    print '****************************************************'
